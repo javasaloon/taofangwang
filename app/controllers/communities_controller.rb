3deb0cb7978eb2d537d89a380d4ba1d0be27fe0b
jacky <javasaloon@gmail.com>
2013-01-03 10:31:29 +0800

    add tabs of districts

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -14,6 +14,7 @@ class CommunitiesController < ApplicationController
   # GET /communities/1.json
   def show
     @community = Community.find(params[:id])
+    @districts = District.all
 
     respond_to do |format|
       format.html # show.html.erb
